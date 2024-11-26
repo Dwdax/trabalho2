33a4a0bfc2c1005eddf92c512b33d9e8e856757e
Dwdax <eduardaolivera759@gmail.com>
2024-11-25 23:41:25 -0300

    Add files via upload

---
+++ trabalho (1).php
@@ -0,0 +1,36 @@
+<?php
+
+$notas = [
+    [
+        "notas" => "Maria",
+        "materias" => [
+            "mat" => [0, 0],
+            "port" => [6, 6],
+            "hist" => [10, 4],
+            "geo" => [7, 6]
+        ]
+    ],
+    [
+        "notas" => "João",
+        "materias" => [
+            "mat" => [10, 6],
+            "port" => [6, 3],
+            "hist" => [1, 4],
+            "geo" => [6, 4]
+        ]
+    ],
+    [
+        "notas" => "Gb",
+        "materias" => [
+            "mat" => [6, 2],
+            "port" => [8, 4],
+            "hist" => [4, 4],
+            "geo" => [6, 8]
+        ]
+    ],
+]
+
+
+
+
+?>
